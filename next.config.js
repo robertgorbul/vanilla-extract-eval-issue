fa0653c38a94007b596a9384ac121aa8cfa74597
Robert Gorbul <rgorbul@gmail.com>
2022-03-23 17:01:57 +0100

    vanilla-extract-eval-issue

---
+++ next.config.js
@@ -0,0 +1,14 @@
+const { createVanillaExtractPlugin } = require('@vanilla-extract/next-plugin');
+
+const withVanillaExtract = createVanillaExtractPlugin();
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    formats: ['image/avif', 'image/webp'],
+  },
+};
+
+module.exports = withVanillaExtract(nextConfig);
